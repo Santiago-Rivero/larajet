91b9c89e8b0b354c7bbc83e62796b729b2350169
Santiago-Rivero <santiagonrivero@gmail.com>
2021-03-16 16:07:31 -0300

    nuevas depencias

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,10 +10,9 @@
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('name') }}" />
+                <x-jet-label for="name" value="{{ __('Name') }}" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
-
 
             <div class="mt-4">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
@@ -49,11 +48,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Esta registrado?') }}
+                    {{ __('Already registered?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Registrese') }}
+                    {{ __('Register') }}
                 </x-jet-button>
             </div>
         </form>
